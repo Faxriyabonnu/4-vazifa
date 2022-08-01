255a0e3968f324f7cb8ba088fea5d250506ce3d6
Faxriyabonnu <bonumirzayeva2002@gmail.com>
2022-08-01 14:27:57 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,183 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+// import logo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav className="my-2">
+        <div className="container px-4 px-lg-5">
+        <a className="navbar-brand" href="#">Start Bootstrap</a>
+      
+        <div className="navbarCollapse">
+          <ul className="navbar me-auto mb-lg-0 ">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">shop</a></li>
+
+          </ul>
+          <form className="d-flex">
+                        <button className="btn btn-outline-dark" type="submit">
+                            <i className="bi-cart-fill me-1"></i>
+                            Cart
+                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+           </form>
+        </div>
+        </div>
+      </nav>
+      <header className="bg-dark py-5">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="text-center text-white">
+            <h1 display-4 fw-bolder>Shop in style</h1>
+            <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+          </div>
+        </div>
+      </header>
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 mt-5">
+          <div className="row gx-4 gx-lg-5 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img src={'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'} alt="yu" />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className='fw-bolder'>Fancy Product</h5>
+                    $40.00 - $80.00          
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className='btn btn-outline-dark mt-auto' href="#">View options</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className='py-5 bg-dark'>
+        <div className="container">
+          <p className="m-0 text-white text-center">
+          Copyright © Your Website 2022
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
